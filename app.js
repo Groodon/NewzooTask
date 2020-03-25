2fa357b6f833dabf7e20411e6475706db28e31f7
Groodon <simondelvert@hotmail.com>
2020-03-25 13:09:20 +0100

    Newzoo task

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const lineByLine = require('n-readlines');
+const fileName = "packages.txt";
+const app = express();
+const port = 3000;
+
+
+
+const fieldsToExtract = ["Package", "Version", "Description", "SHA256", "Dependencies", "Recommends", "conflicts"];
+
+
+function findPackage(packageName) {
+    const liner = new lineByLine(fileName);
+    let line;
+    let packageFound = false;
+    let res = {};
+    let field, value;
+
+    while (line = liner.next()) {
+        line = line.toString('ascii');
+        if (line.length !== 0) {
+            [field, value] = line.split(":", 2);
+
+            if ((field === "Package" && value.trim() === packageName.trim()) || packageFound === true) {
+                // We found it!
+                packageFound = true;
+                if (fieldsToExtract.includes(field)) {
+                    res[field] = value.trim();
+                }
+            }
+        } else if (packageFound) {
+            return res;
+        }
+    }
+}
+
+//findPackage("apt");
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/search/:name', (req, res) => {
+    res.json(findPackage(req.params.name));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
